9b20760f6cb4cdbfc209489f33a5ffa7bc6ebc81
Nayan Mahato <nayanmahato904@gmail.com>
2026-05-28 18:13:39 +0530

    note creation

---
+++ src/services/note.service.js
@@ -0,0 +1,19 @@
+const noteModel = require("../models/notes.model");
+const ApiError = require("../utils/apiError");
+
+const registerService = async (data) => {
+  const { title, description } = data;
+
+  if(!title || !description){
+    throw new ApiError(200 , "all fields are required")
+  }
+  
+  const newNote = await noteModel.create({
+    title,
+    description,
+  });
+
+  return newNote;
+};
+
+module.exports = { registerService };
