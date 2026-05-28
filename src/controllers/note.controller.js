823f505db84763cfaad701930a4565ca4c438edc
Nayan Mahato <nayanmahato904@gmail.com>
2026-05-28 18:36:57 +0530

    read note added

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -11,6 +11,13 @@ const createNoteController = asyncHandler(async(req,res)=>{
   )
 })
 
+const readNoteController = asyncHandler(async(req,res)=>{
+  let result = await noteReadService(req.body)
+
+  return res.status(201).json(
+    new ApiResponse("user" , "user registered sucessfully")
+  )
+})
 module.exports = {
     createNoteController
 }
